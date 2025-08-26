94ea28ed4efae779d9d21b66c0e645b59af6d2fc
Aurialedge <saperkar1862004@gmail.com>
2025-08-26 22:53:15 +0530

    Reduce fn

--- Python_learn/chapter_13/04_reduce.py
+++ Python_learn/chapter_13/04_reduce.py
@@ -7,3 +7,21 @@ nums = [1, 2, 3, 4, 5]
 
 product = reduce(lambda x, y: x * y, nums)
 print(product)   # 120  (1*2*3*4*5)
+
+# more examples
+# Find sum of all numbers
+sum = reduce(lambda x, y: x + y, nums)
+print(sum)      # 15   (1+2+3+4+5)
+
+# Find maximum number
+max_num = reduce(lambda x, y: x if x > y else y, nums)
+print(max_num)  
+# more examples
+# Find minimum number
+min_num = reduce(lambda x, y: x if x < y else y, nums)
+print(min_num)  # 1
+
+# complex example
+# Find the product of all even numbers
+even_product = reduce(lambda x, y: x * y if y % 2 == 0 else x, nums, 1)
+print(even_product)  # 8 (2*4)
